fdcdda9f83f0117b2d106dae1863ba90def32e6b
Burgerballs <107233412+Burgerballs@users.noreply.github.com>
2025-01-01 13:50:40 +0000

    hmm

--- source/funkin/states/MainMenuState.hx
+++ source/funkin/states/MainMenuState.hx
@@ -115,7 +115,7 @@ class MainMenuState extends MusicBeatState
 			menuItems.add(menuItem);
 		}
 
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, 'Troll Engine ' + Main.Version.displayedVersion, 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, 'TE ' + Main.Version.displayedVersion, 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
