49d19506efb884b57a3ac200753477b7a27b2f88
Burgerballs <107233412+Burgerballs@users.noreply.github.com>
2025-02-10 14:15:21 +0000

    penis

--- setup/install_haxelibs_unix.sh
+++ setup/install_haxelibs_unix.sh
@@ -5,7 +5,6 @@ haxelib --always --quiet git funkin.vis https://github.com/FunkinCrew/funkVis
 haxelib --always --quiet git grig.audio https://gitlab.com/haxe-grig/grig.audio.git
 haxelib --always --quiet install flixel 5.5.0
 haxelib --always --quiet install openfl 9.3.4
-
 haxelib --always --quiet install flixel 5.6.2
 haxelib --always --quiet install flixel-ui 2.6.1
 haxelib --always --quiet install flixel-addons 3.2.3
