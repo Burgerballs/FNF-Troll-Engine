fef0d7a193f8d8ec6103f7b27e0863afa00daa5a
Burgerballs <107233412+Burgerballs@users.noreply.github.com>
2025-04-17 19:04:14 +0100

    Update windows-haxelibs.bat

--- setup/windows-haxelibs.bat
+++ setup/windows-haxelibs.bat
@@ -6,7 +6,7 @@ haxelib --always --quiet git openfl https://github.com/swordcubes-grave-of-shite
 haxelib --always --quiet install flixel-ui 2.6.2
 haxelib --always --quiet install flixel-addons 3.3.2
 haxelib --always --quiet git hscript https://github.com/nebulazorua/t-hscript
-haxelib --always --quiet install hxvlc 1.6.1
+haxelib --always --quiet install hxvlc 2.0.1
 haxelib --always --quiet install hxdiscord_rpc 1.1.1
 haxelib --always --quiet git linc_luajit https://github.com/riconuts/linc_luajit
 haxelib --always --quiet install moonchart 0.4.0
